1e9a4740507dd884f6054897c3309691c38bf8b4
cyber-claws <ishmaelsibisi@gmail.com>
2021-10-14 08:19:00 +0200

    Update manifest and add first version of dashboards.

--- snapt/datadog_checks/snapt/config_models/validators.py
+++ snapt/datadog_checks/snapt/config_models/validators.py
@@ -1,3 +1,9 @@
 # (C) Datadog, Inc. 2021-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+
+def initialize_instance(values, **kwargs):
+    if 'url' not in values:
+        raise ValueError('Field `url` is required')
+
+    return values
