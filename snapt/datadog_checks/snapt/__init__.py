6527821833dbff42dd3e38cd1ad0cd4770310af1
cyber-claws <ishmaelsibisi@gmail.com>
2021-10-14 07:44:23 +0200

    Include accelerator and balancer basic checks

--- snapt/datadog_checks/snapt/__init__.py
+++ snapt/datadog_checks/snapt/__init__.py
@@ -1,7 +1,15 @@
-# (C) Datadog, Inc. 2021-present
+# (C) Datadog, Inc. 2018-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from .__about__ import __version__
-from .check import SnaptCheck
+from .balancer.haproxy import BalancerCheck
+from .accelerator.nginx import AcceleratorCheck
+from .metrics import VTS_METRIC_MAP
 
-__all__ = ['__version__', 'SnaptCheck']
+
+__all__ = [
+    '__version__',
+    'BalancerCheck',
+    'AcceleratorCheck',
+    'VTS_METRIC_MAP',
+]
